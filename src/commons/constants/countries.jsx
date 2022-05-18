08df74747c417f244fd1c3dad4b969a8974df2a5
Lerpardo <ls6182315@gmail.com>
2022-05-18 12:55:23 -0300

    Adição de novos países

--- src/commons/constants/countries.jsx
+++ src/commons/constants/countries.jsx
@@ -1,8 +1,15 @@
 import BrazilFlag from '../../assets/images/brazil.png'
 import ChinaFlag from '../../assets/images/china.png'
+import FranceFlag from '../../assets/images/france.png'
 import GermanyFlag from '../../assets/images/germany.png'
 import ItalyFlag from '../../assets/images/italy.png'
+import MexicoFlag from '../../assets/images/mexico.png'
+import SpainFlag from '../../assets/images/spain.png'
+import ThailandFlag from '../../assets/images/thailand.png'
+import TurkeyFlag from '../../assets/images/turkey.jpg'
+import UkFlag from '../../assets/images/uk.png'
 import UsaFlag from '../../assets/images/usa.png'
+
 
 const COUNTRIES = [
   {
@@ -26,9 +33,39 @@ const COUNTRIES = [
     flag: UsaFlag
   },
   {
+    label:'França',
+    value: 'france',
+    flag: FranceFlag
+  },
+  {
     label: 'Itália',
     value: 'italy',
     flag: ItalyFlag
+  },
+  {
+    label:'México',
+    value:'mexico',
+    flag: MexicoFlag
+  },
+  {
+    label: 'Reino Unido',
+    value: 'uk',
+    flag: UkFlag
+  },
+  {
+    label: 'Espanha',
+    value: 'spain',
+    flag: SpainFlag
+  },
+  {
+    label: 'Tailândia',
+    value: 'thailand',
+    flag: ThailandFlag
+  },
+  {
+    label: 'Turquia',
+    value: 'turkey',
+    flag: TurkeyFlag
   }
 ]
 
